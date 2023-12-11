3efa25d567e8364ef039e587efd78da2b6996174
Vicente <64921519+VicenteM23@users.noreply.github.com>
2023-12-11 18:00:09 +0100

    Ejemplos actualizados

--- Ejemplo 1/EjemploCodigoLimpio1.java
+++ Ejemplo 1/EjemploCodigoLimpio1.java
@@ -17,28 +17,19 @@ public class EjemploCodigoLimpio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        //Realizamos el proceso para solicitar al usuario que nos indique cuantos aciertos ha obtenido en su examen:
+            Scanner scan = new Scanner(System.in);
             String numeroAciertos;
             System.out.print("Inserta el número de aciertos en 20 preguntas :" );
             numeroAciertos = scan.nextLine();
-            double numAciertosDouble = Double.valueOf(numeroAciertos); /*Necesitamos realizar el cambio de tipo, 
-            ya que, hemos solicitado un String y nosotros vamos a trabajar con double*/
+            double numAciertosDouble = Double.valueOf(numeroAciertos);
             
-        /*Creamos y visualizamos por pantalla la variable que contiene el número de errores 
-        que el usuario ha obtenido:*/
-            double numeroErrores = 20-numAciertosDouble;
+            double numeroErrores = 20-numAciertosDouble; //Operación que calcula el número de errores
             System.out.println("Número de errores: " + numeroErrores);
             
-        /*Debido a que cada error resta 0.5 a la nota, es necesario dividir sus errores entre 2 
-        para obtener cuanta nota total se le restará, dicho valor lo almacenamos en otra variable:*/
-            double notaResta = numeroErrores / 2;
+            double notaResta = numeroErrores / 2; //Operación que calcula la nota que restará a la final
             System.out.println(notaResta);
             
-        /*Creamos nuestra última variable, que nos servirá para realizar nuestro condicional if, 
-        este le mostrará al usuario su nota numérica además de indicarle si esta es Insuficiente, 
-        Suficiente, Bien, Notable o Sobresaliente*/
-            double notaFinal = (numAciertosDouble - notaResta)/2;
+            double notaFinal = (numAciertosDouble - notaResta)/2; //Operación para calcular la nota final
             
             if(notaFinal < 5)
                 System.out.println("Calificación: " + notaFinal + " Insuficiente");
